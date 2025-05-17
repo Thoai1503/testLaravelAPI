9230415dce2fd9014f3777b8ebcb36ab04ec3b13
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-05-17 21:38:33 +0700

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $fillable = ['name', 'description'];
+
+   
+}
+?>
