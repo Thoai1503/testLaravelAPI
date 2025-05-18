1289293254c8cbc6c6faf070202c3e5f3b61e22f
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-05-18 11:40:38 +0700

    add image

---
+++ public/image/1737976975.php
@@ -0,0 +1,50 @@
+
+<?php
+$truonghoc = [
+    "class01" => [
+        "st01" => [
+            "name" => "Vũ Bê",
+            "age" => 14,
+            "gender" => 3
+        ],
+        "st02" => [
+            "name" => "Bảo Bê",
+            "age" => 30,
+            "gender" => 3
+        ],
+        "st01" => [
+            "name" => "Tèo Bê",
+            "age" => 60,
+            "gender" => 3
+        ],
+        "st01" => [
+            "name" => "Hòa Bóng",
+            "age" => 70,
+            "gender" => 3
+        ]
+    ],
+    "class02" => [
+        "st05" => [
+            "name" => "Tèo Bê",
+            "age" => 50,
+            "gender" => 3
+        ],
+        "st06" => [
+            "name" => "Tí Bê",
+            "age" => 50,
+            "gender" => 3
+        ],
+        "st07" => [
+            "name" => "Tủn Bê",
+            "age" => 60,
+            "gender" => 3
+        ],
+        "st08" => [
+            "name" => "J97 Bóng",
+            "age" => 40,
+            "gender" => 3
+        ]
+    ]
+        ];
+      
+        json_encode($truonghoc);
